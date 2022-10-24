13b0b451c72fb827c2e287cb1f20576fded158a7
Vaibhav <vmdeshmukh333@gmail.com>
2022-10-25 00:23:18 +0530

    adding LOM all files

---
+++ config.py
@@ -0,0 +1,13 @@
+import os 
+
+PORT = 7007
+
+MODEL_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS/Project_app/LogisticModelIris.pkl'
+JSON_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS/Project_app/Project_data.json'
+
+
+
+
+
+# os.path.join('Project_app','Project_data')
+# os.path.join('Project_app','LinearModelIris')
