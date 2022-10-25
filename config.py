5da3983b1e8052c8fbfab3ea938d19769b317dfa
Vaibhav <vmdeshmukh333@gmail.com>
2022-10-25 17:40:08 +0530

    commiting all the files

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 PORT = 7007
 
-MODEL_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS/Project_app/LogisticModelIris.pkl'
-JSON_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS/Project_app/Project_data.json'
+MODEL_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS_SPECIES/Project_app/LogisticModelIris.pkl'
+JSON_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS_SPECIES/Project_app/Project_data.json'
 
 
 
--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 # API'S
+from ast import Return
+from unittest import result
 from flask import Flask,render_template,jsonify,request
 #importing the required functions from flask
 
@@ -18,25 +20,43 @@ app = Flask(__name__)
 @app.route('/')
 def Hello_Flask():
     print('Welcome to the Base API')
-    return 'HI THIS BASE API'
+    return render_template('index.html')
 
 
-@app.route('/predict_species')
-def get_predicted_species():    
+
+# @app.route('/predict_species')
+# def get_predicted_species():    
     
-    SepalLengthCm   = 6.3 
-    SepalWidthCm    = 3.3
-    PetalLengthCm   = 6 
-    PetalWidthCm    = 2.5
+#     SepalLengthCm   = 6.3 
+#     SepalWidthCm    = 3.3
+#     PetalLengthCm   = 6 
+#     PetalWidthCm    = 2.5
 
-    #creating instance of class
+#     #creating instance of class
+#     fl = Flower(SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm)
+#     species = fl.get_predicted_species()
+    
+#     return jsonify({'Result':f'The Logistic Model has Predicted the Species  of the test data as: {species}'})
+    
+@app.route('/submit',methods = ['POST','GET'])
+def submit():   
+    
+    if request.method == 'POST':
+        
+        SepalLengthCm   = float(request.form['SepalLengthCm'])       
+        SepalWidthCm    = float(request.form['SepalWidthCm'])
+        PetalLengthCm   = float(request.form['PetalLengthCm'])
+        PetalWidthCm    = float(request.form['PetalWidthCm'])
+    
     fl = Flower(SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm)
     species = fl.get_predicted_species()
     
-    return jsonify({'Result':f'The Logistic Model has Predicted the Species  of the test data as: {species}'})
-    
+    # return jsonify({'Result':f'The Logistic Model has Predicted the Species  of the test data as: {species}'})
+    return render_template('result.html',result = species)
+
 
 if __name__ == '__main__':
     app.run()                     
 # it Allows You to Execute Code When the File Runs as a Script,
 # but Not When It's Imported as a Module.
+
